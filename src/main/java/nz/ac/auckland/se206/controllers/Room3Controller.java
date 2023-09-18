5a28475e9a5219909086f02ef01791d35de141ba
ben-g-man <bhum783@aucklanduni.ac.nz>
2023-09-19 11:35:13 +1200

    feat - Added correct and incorrect pin code functionality

--- src/main/java/nz/ac/auckland/se206/controllers/Room3Controller.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Room3Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.App;
@@ -20,30 +21,40 @@ public class Room3Controller extends GptInteraction {
   @FXML private Rectangle pinPadOpen;
   @FXML private Rectangle moveRoom2;
   @FXML private Label pinHintText;
+  @FXML private GridPane pinPad;
+  @FXML private Button pinDigit0;
+  @FXML private Button pinDigit1;
+  @FXML private Button pinDigit2;
+  @FXML private Button pinDigit3;
+  @FXML private Button pinDigit4;
+  @FXML private Button pinDigit5;
+  @FXML private Button pinDigit6;
+  @FXML private Button pinDigit7;
+  @FXML private Button pinDigit8;
+  @FXML private Button pinDigit9;
+  @FXML private Button pinRemove;
   @FXML private Button pinSubmit;
   @FXML private Pane pinPadUi;
+  private boolean pinPadReady = true;
 
   private String[] pinHints = {
     "Item 0", "Item 1", "Item 2", "Item 3", "Item 4",
     "Item 5", "Item 6", "Item 7", "Item 8", "Item 9"
   };
-  private int[] pin;
+  private String pin;
 
   @FXML
   public void initialize() throws ApiProxyException {
-    pin = new int[4];
-    for (int i = 0; i < 4; i++) {
-      pin[i] = (int) (Math.random() * 9);
-    }
+    pin = Integer.toString((int) (Math.random() * 9999));
 
     pinHintText.setText(
-        pinHints[pin[0]]
-            + ", "
-            + pinHints[pin[1]]
-            + ", "
-            + pinHints[pin[2]]
-            + ", "
-            + pinHints[pin[3]]);
+        pinHints[pin.charAt(0) - 48]
+            + "\n"
+            + pinHints[pin.charAt(1) - 48]
+            + "\n"
+            + pinHints[pin.charAt(2) - 48]
+            + "\n"
+            + pinHints[pin.charAt(3) - 48]);
   }
 
   @FXML
@@ -59,6 +70,34 @@ public class Room3Controller extends GptInteraction {
   @FXML
   public void clickPinPadClose(MouseEvent event) throws IOException {
     pinPadUi.setVisible(false);
+  }
+
+  @FXML
+  public void pinDigitClick(MouseEvent event) throws IOException {
+    if ((pinPadReady) && (pinTextField.getLength() < 4)) {
+      Button eventSource = (Button) event.getSource();
+      String pressedDigit = eventSource.getText();
+      pinTextField.appendText(pressedDigit);
+    }
+  }
+
+  @FXML
+  public void pinRemoveClick(MouseEvent event) throws IOException {
+    if ((pinPadReady) && (pinTextField.getLength() > 0)) {
+      pinTextField.setText(pinTextField.getText(0, pinTextField.getLength() - 1));
+    }
+  }
+
+  @FXML
+  public void pinSubmitClick(MouseEvent event) throws IOException {
+    if (pinPadReady) {
+      pinPadReady = false;
+      if (pinTextField.getText().equals(pin)) {
+        pinTextField.setText("Password Correct! Unlocking...");
+      } else {
+        pinTextField.setText("Password Incorrect!");
+      }
+    }
   }
 
   @Override
