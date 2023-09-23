9017bebf5f1c2febb075debf3653672c8385ae9a
ben-g-man <bhum783@aucklanduni.ac.nz>
2023-09-23 17:01:06 +1200

    fix - fixed comile issue

---
+++ src/main/java/nz/ac/auckland/se206/controllers/WinController.java
@@ -0,0 +1,19 @@
+package nz.ac.auckland.se206.controllers;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.SceneManager;
+
+public class WinController {
+
+  @FXML private Button menuBtn;
+
+  @FXML
+  public void menu(MouseEvent event) throws IOException {
+    App.setRoot(SceneManager.AppUI.INTRO);
+    App.resetGame();
+  }
+}
