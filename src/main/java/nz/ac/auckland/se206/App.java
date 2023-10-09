1bd1dfc4a203f3edd35bab3c00da58cabe722fa2
Ben-G-Man <bhum783@aucklanduni.ac.nz>
2023-10-09 16:47:22 +1300

    feat - Added smooth scrolling between rooms

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -18,6 +18,8 @@ import nz.ac.auckland.se206.speech.TextToSpeech;
  */
 public class App extends Application {
 
+  public static final int ROOM_WIDTH = 960;
+
   private static Scene scene;
   public static TextToSpeech textToSpeech;
   public static Room1Controller room1;
--- src/main/java/nz/ac/auckland/se206/controllers/InGameController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/InGameController.java
@@ -1,8 +1,13 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.SubScene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
 
@@ -14,6 +19,8 @@ public class InGameController {
   @FXML private SubScene room3;
   @FXML private Pane roomSlider;
 
+  Boolean moveEnabled = true;
+
   @FXML
   public void initialize() throws ApiProxyException {
     topBar.setRoot(SceneManager.getUI(SceneManager.AppUI.TOPBAR));
@@ -21,5 +28,24 @@ public class InGameController {
     room1.setRoot(SceneManager.getUI(SceneManager.AppUI.ROOM1));
     room2.setRoot(SceneManager.getUI(SceneManager.AppUI.ROOM2));
     room3.setRoot(SceneManager.getUI(SceneManager.AppUI.ROOM3));
+  }
+
+  @FXML
+  public void moveLeft(MouseEvent event) throws IOException {
+    moveRoomSlider(App.ROOM_WIDTH);
+  }
+
+  @FXML
+  public void moveRight(MouseEvent event) throws IOException {
+    moveRoomSlider(-App.ROOM_WIDTH);
+  }
+
+  public void moveRoomSlider(int distance) {
+    if (moveEnabled) {
+      moveEnabled = false;
+      TranslateTransition moveRooms = new TranslateTransition(Duration.millis(600), roomSlider);
+      moveRooms.setByX(distance);
+      moveRooms.play();
+    }
   }
 }
